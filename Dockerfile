2bf795c885dd1cf652a716c2f597aa2149eab54a
Devansh Chaudhary <devanshchaudhary2002@gmail.com>
2023-10-28 12:14:21 +0530

    wd

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,5 @@ FROM python:3.11
 WORKDIR /app
 COPY . .
 RUN pip install -r requirements.txt
-EXPOSE 80
+EXPOSE 8080
 CMD [ "python", "app.py" ]
--- app.py
+++ app.py
@@ -166,5 +166,5 @@ def register_user():
 
 	
 if __name__ == '__main__':
-	app.run(host="0.0.0.0", port=80)
+	app.run(host="0.0.0.0", port=8080)
 	# app.run()
